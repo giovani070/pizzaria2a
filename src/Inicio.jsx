647a7b1b5c88681fbf9f684782ee92c4db3376fa
giovani070 <bgiovani227@gmail.com>
2024-06-10 08:31:28 -0300

    alterações

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -3,6 +3,12 @@ const Inicio = () => {
     return (
         <>
             <h2>Página de Início</h2>
+            <p>Aula de ReactsJS</p>
+            <input type="text" id="texto" />
+            <input type="button" id="botão" 
+              value="CLIQUE AQUI"
+              onClick={()=>alert('Ola Mundo')}
+              />
         </>
     )
 }
